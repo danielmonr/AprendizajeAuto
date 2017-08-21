dee78a5fd8785dc6a1d448379ffac08a61de720f
Monzalvo <danielmonr@gmail.com>
2017-08-21 17:19:54 -0500

    Final program, proy1

--- 1proy/regresionlineal.py
+++ 1proy/regresionlineal.py
@@ -2,32 +2,22 @@ import numpy
 import matplotlib.pyplot as plt
 
 def leerDatos(file):
-    print 'Parsing File'
     points = numpy.loadtxt(file, delimiter=',') # Save all the file's points in a Matrix
-    #print points.shape
     x = numpy.r_['1,2,0', numpy.ones(points.shape[0]), points[:, 0]] # Create Matrix of Xs with 1s
     y = numpy.r_['0,2,0', points[:, 1]] # Create Y Vector, as a Matrix
-    #print x
-    #print y
-    print 'Dim x:'
-    print x.shape
-    print 'Dim y:'
-    print y.shape
     return x,y
     
 def graficaDatos(X,y,theta):
-    #points = numpy.r_['1,2,0',X[:,1], y[:,0]]
     Opoints = X.dot(theta)
-    plt.plot(X,y,'b',X,Opoints,'r')
+    plt.plot(X,y,'b^',X,Opoints,'r')
     plt.show()
     return 0
     
 def gradienteDescendente(X,y, theta = numpy.zeros((2,1)),alpha =  0.01,iteraciones = 1500):
     m = y.size # Numero de datos
-    for i in xrange(0,iteraciones):
-        theta = theta - alpha * (1.0/m) * (numpy.dot(X.T,(numpy.dot(X,theta) - y)))
+    for i in range(0,iteraciones):
+        theta = theta - alpha * (1.0/m) * (numpy.dot(X.T,(numpy.dot(X,theta) - y))) # Theta vector for each iteration
     
-    #print theta
     return theta
     
 def calculaCosto(X,y,theta):
@@ -38,14 +28,12 @@ def calculaCosto(X,y,theta):
 
 X,Y = leerDatos("./ex1data1.txt")
 O = gradienteDescendente(X,Y) # Save final theta in O
-print 'Dim O'
-print O.shape
-print "\nTheta vector:"
-print O
-print "\nPrediccion 1:"
-print numpy.array([1, 3.5]).dot(O)
-print "Prediccion 2:"
-print numpy.array([1,7]).dot(O)
-print "Cost:"
-print calculaCosto(X,Y,O)
-#graficaDatos(X,Y,O)
+print("\nTheta vector:")
+print(O)
+print("\nPrediccion 1:")
+print(numpy.array([1, 3.5]).dot(O))
+print("Prediccion 2:")
+print(numpy.array([1,7]).dot(O))
+print("Cost:")
+print(calculaCosto(X,Y,O))
+graficaDatos(X,Y,O)
